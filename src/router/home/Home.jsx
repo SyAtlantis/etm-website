77048e120b8244083c204df9eb66de7cd4e7327f
SyAtlantis <1102960695@qq.com>
2018-05-08 22:56:15 +0800

    etm

--- src/router/home/Home.jsx
+++ src/router/home/Home.jsx
@@ -7,13 +7,13 @@ import './Home.less';
 import EtmHeader from '../../components/header/Header';
 import EtmFooter from '../../components/footer';
 import Banner from '../../components/home-banner';
-// import Section1 from '../../components/home-section1';
-// import Section2 from '../../components/home-section2';
-// import Section3 from '../../components/home-section3';
-// import Section4 from '../../components/home-section4';
-// import Section5 from '../../components/home-section5';
-// import Section6 from '../../components/home-section6';
-// import Section7 from '../../components/home-section7';
+import Section1 from '../../components/home-section1';
+import Section2 from '../../components/home-section2';
+import Section3 from '../../components/home-section3';
+import Section4 from '../../components/home-section4';
+import Section5 from '../../components/home-section5';
+import Section6 from '../../components/home-section6';
+import Section7 from '../../components/home-section7';
 const { Header, Content, Footer } = Layout;
 class Home extends React.Component {
     // constructor(props, context) {
@@ -42,15 +42,22 @@ class Home extends React.Component {
     // }
     render() {
         return (
-            <Layout className="layout">
-                <Header>
+            <Layout className="home_layout">
+                <Header className="home_header">
                     <EtmHeader />
                 </Header>
-                <Content>
-                    <Banner/>
+                <Content className="home_content">
+                    <Banner />
+                    <Section1 history={this.props.history} />
+                    <Section2 history={this.props.history} />
+                    <Section3 history={this.props.history} />
+                    <Section4 history={this.props.history} />
+                    <Section5 history={this.props.history} />
+                    <Section6 history={this.props.history} />
+                    <Section7 history={this.props.history} />
                 </Content>
-                <Footer>
-                    <EtmFooter/>
+                <Footer className="home_footer">
+                    <EtmFooter />
                 </Footer>
             </Layout>
         )
